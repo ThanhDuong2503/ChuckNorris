099b732c6da8670939447474c84e820de4ce4dbc
Thanh <Thanh.Duong2503@gmx.de>
2020-06-21 16:17:37 +0200

    Add Categories

--- src/Paths/Categories.js
+++ src/Paths/Categories.js
@@ -1,8 +1,22 @@
 import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 export default function Categories() {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.chucknorris.io/jokes/categories")
+            .then (response => response.json())
+            .then (data => setCategories(data))
+    },[] );
+
 return(
-<h1>hi</h1>
+    <div>
+        {categories.map(category => (
+            <h2>{category}</h2>
+        ))}
+    </div>
 )
 }
 
